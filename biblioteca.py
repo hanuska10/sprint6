28515ca7f38a3d1f8596783bdf1cf0dd49f1ab97
hanuska10 <57403582+hanuska10@users.noreply.github.com>
2020-01-30 15:53:37 +0100

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -7,16 +7,7 @@ class Biblioteca:
 
 #metodo
 
-     def menu(self):
-            resp = True
-            print("                           M E N Ú :bookmark_tabs::books:")
-            x = input("Si desea consultar, escribe 'c' o si desea agregar, escribe 'a':  ")
-                    if x == "c":
-                        self.consultar()
-                    elif x == "a":
-                        self.menuAgregar()
-                    else:
-                        x = input("Debe escribir 'c' para consullar ú 'a' para agregar:  ")
+    
 
     def crearGuardar(self):
 
