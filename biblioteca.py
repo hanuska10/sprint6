695ee020188c581b561af3ae372d4163b430207f
hanuska10 <57403582+hanuska10@users.noreply.github.com>
2020-01-31 13:06:47 +0100

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -1,50 +1,23 @@
+import os
 class Biblioteca:
-    titulo=""
-    autor=""
-    fechaEd=""
-    editorial=""
 
-
-#metodo
-
-    
-
+    #metodo
     def crearGuardar(self):
 
-        print("entro")
-        self.titulo = input("introduce titulo ")
-        self.autor = input("introduce autor ")
-        self.fechaEd = input("introduce fecha de edicion ")
-        self.editorial = input("introduce editorial ")
+        self.titulo = str(input("Escribe el título del libro: "))
+        self.autor = str(input("Escribe el nombre del autor: "))
+        self.fechaEd = str(input("Escribe el año de publicación: "))
+        self.editorial = str(input("Escribe el nombre de la editoria: "))
 
-        f = open("mifish.txt", "a")
-        f.write(self.titulo + '\n' )
-        f.write(self.autor + '\n' )
-        f.write(self.fechaEd + '\n' )
+        f = open("lista_libros.txt", "a")
+        f.write(self.titulo + '\n')
+        f.write(self.autor + '\n')
+        f.write(self.fechaEd + '\n')
         f.write(self.editorial + '\n' + '\n')
 
-    def menuAgregar(self):
-        resp = True
-        x = input("¿Desea agregar otro libro? Escribe si o no: ")
-        while(resp):
-            if x == "si":
-                self.crearGuardar()
-            elif x == "no":
-                resp = False
-                self.menu()
-            else:
-                x = input("¿Desea agregar otro libro? Escribe si o no: ")
-
     def consultar(self):
-
-        f = open("mifish.txt", "r")
-        for x in f:
-            print(x)
-
-
-#objeto
-
-libro1= Biblioteca()
-#libro1.crearGuardar()
-#libro1.consultar()
-libro1.menu()
+        if os.path.isfile('C:/Users/formacion/Documents/GitHub/sprint6/lista_libros.txt'):
+            f = open("lista_libros.txt", "r")  # lee el archivo si existe
+            return f
+        else:
+            print('El no archivo existe.')
