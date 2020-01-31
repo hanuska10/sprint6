1be67f3056357519218eda81e37ce314046adb05
hanuska10 <57403582+hanuska10@users.noreply.github.com>
2020-01-31 13:50:51 +0100

    Create main.py

---
+++ draft/main.py
@@ -0,0 +1,36 @@
+from biblioteca import Biblioteca
+
+
+class MenuLibros:
+
+    def init(self):
+        self.libro1 = Biblioteca()
+
+    def mostrarMenu(self):
+
+        print(30 * "-", "MENU", 30 * "-")
+        print("1. Menu consultar 1")
+        print("2. Menu agregar 2")
+        print("3. Exit")
+        print(67 * "-")
+
+        menu=True
+
+        while menu:
+            #self.mostrarMenu()
+            opcion = int(input(" Seleccione una opción [1-3:] "))
+
+            if opcion == 1:
+                self.libro1.consultar()
+            elif opcion == 2:
+                self.libro1.crearGuardar()
+            elif opcion == 3:
+                menu = False
+            else:
+                input("Wrong option selection. Enter any key to try again..")
+
+        print("Fin")
+
+
+lista = MenuLibros()
+lista.mostrarMenu()
