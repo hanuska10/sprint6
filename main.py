b8148741e8f0c538884481300d9dd9c49644ec2d
hanuska10 <57403582+hanuska10@users.noreply.github.com>
2020-01-31 13:06:43 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-from biblioteca 
+from biblioteca import Biblioteca
 
-def menu(self):
-        #resp = True
-        print("                           M E N Ú :bookmark_tabs::books:")
-        x = input("Si desea consultar, escribe 'c' o si desea agregar, escribe 'a':  ")
-        if x == "c":
-            self.consultar()
-        elif x == "a":
-            self.menuAgregar()
-        else:
-            x = input("Debe escribir 'c' para consullar ú 'a' para agregar:  ")
+
+class MenuLibros:
+
+    def init(self):
+        self.libro1 = Biblioteca()
+
+    def mostrarMenu(self):
+
+        print(30 * "-", "MENU", 30 * "-")
+        print("1. Menu Option 1")
+        print("2. Menu Option 2")
+        print("3. Exit")
+        print(67 * "-")
+
+        menu=True
+
+        while menu:
+            #self.mostrarMenu()
+            opcion = int(input(" Seleccione una opción [1-3:] "))
+
+            if opcion == 1:
+                self.libro1.consultar()
+            elif opcion == 2:
+                self.libro1.crearGuardar()
+            elif opcion == 3:
+                menu = False
+            else:
+                input("Wrong option selection. Enter any key to try again..")
+
+        print("Fin")
+
+
+lista = MenuLibros()
+lista.mostrarMenu()
