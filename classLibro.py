5d642300574bdf4cad7484c99c6e4effe1fb0f0b
laliiosorio <57403782+laliiosorio@users.noreply.github.com>
2020-02-03 16:32:06 +0100

    Update classLibro.py

--- classLibro.py
+++ classLibro.py
@@ -7,15 +7,22 @@ class Libro:
     def crearGuardar(self):
 
         self.titulo = str(input("Escribe el título del libro: "))
-        self.autor = str(input("Escribe el nombre del autor: "))
-        self.fechaEd = str(input("Escribe el año de publicación: "))
-        self.editorial = str(input("Escribe el nombre de la editoria: "))
-
-        f = open("lista_libros.txt", "a")
-        f.write("Título: "+self.titulo + '\n')
-        f.write("Autor: " + self.autor + '\n')
-        f.write("Fecha edición: "+self.fechaEd + '\n')
-        f.write("Editorial: "+self.editorial + '\n')
+        
+        f = open("lista_libros.txt", "r") 
+        f=f.read()
+        consulta=f.find(self.titulo)
+        if consulta < 0:
+            self.autor = str(input("Escribe el nombre del autor: "))
+            self.fechaEd = str(input("Escribe el año de publicación: "))
+            self.editorial = str(input("Escribe el nombre de la editoria: "))                  
+         
+            f = open("lista_libros.txt", "a")
+            f.write("Título: "+self.titulo + '\n')
+            f.write("Autor: " + self.autor + '\n')
+            f.write("Fecha edición: "+self.fechaEd + '\n')
+            f.write("Editorial: "+self.editorial + '\n')
+        else:
+            print("Libro Existe")
 
     def consultar(self):
         if os.path.isfile('C:/Users/formacion/Documents/GitHub/sprint6/lista_libros.txt'):
