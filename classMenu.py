30d7e546da7adcd5d7c536b71240dc0637e59bae
laliiosorio <57403782+laliiosorio@users.noreply.github.com>
2020-02-05 17:48:50 +0100

    Update classMenu.py

--- classMenu.py
+++ classMenu.py
@@ -5,32 +5,34 @@ class MenuLibros:
     
     def __init__(self):
         self.libro1 = Libro()
-
+        
+        
     def mostrarMenu(self):  
 
         print(30 * "-", "MENU", 30 * "-")
-        print("1. Menu consultar 1")
-        print("2. Menu agregar 2")
-        print("3. Exit")
+        print("1.Consultar Lista de Libros")
+        print("2.Consultar por título")
+        print("3.Agregar nuevo libro a la lista ")
+        print("4. Exit / Salir")
         print(67 * "-")
     
         menu=True
-    
         while menu:
             #self.mostrarMenu()
             opcion = int(input(" Seleccione una opción [1-3:] "))
             print("\n")
-           
         
             if opcion == 1:
                 print(20 * "-", "LISTA LIBROS",20 * "-")
-                self.libro1.consultar()                             
+                self.libro1.consultarLista()
                 print("\n")
                 print(20 * "-")
             elif opcion == 2:
-                self.libro1.crearGuardar()
+                self.libro1.consultarLibro()
             elif opcion == 3:
-                menu = False
+                self.libro1.crearGuardar()
+            elif opcion == 4:
+                menu = False               
             else:
                 input("Wrong option selection. Enter any key to try again..")
 
