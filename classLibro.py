05bca897a7fec1d68747fd693732912f90bcd62a
laliiosorio <57403782+laliiosorio@users.noreply.github.com>
2020-02-05 17:48:37 +0100

    Update classLibro.py

--- classLibro.py
+++ classLibro.py
@@ -1,9 +1,11 @@
 import os
 
-
 class Libro:
+    
+    
+    def __init__(self):
+        self.matriz=[]
 
-    #metodo
     def crearGuardar(self):
 
         self.titulo = str(input("Escribe el título del libro: ")).upper()
@@ -12,22 +14,56 @@ class Libro:
         f=f.read()
         consulta=f.find(self.titulo)
         if consulta < 0:
-            self.autor = str(input("Escribe el nombre del autor: "))
-            self.fechaEd = str(input("Escribe el año de publicación: "))
-            self.editorial = str(input("Escribe el nombre de la editoria: "))                  
+            self.autor = str(input("Escribe el nombre del autor: ")).capitalize()
+            self.fechaEd = str(input("Escribe el año de publicación: ")).capitalize()                
          
             f = open("lista_libros.txt", "a")
             f.write("Título: "+self.titulo + '\n')
             f.write("Autor: " + self.autor + '\n')
             f.write("Fecha edición: "+self.fechaEd + '\n')
-            f.write("Editorial: "+self.editorial + '\n')
+            f.write('\n')
         else:
             print("Libro Existe")
+            
+    def cargarMatriz(self):
+        file = open("lista_libros.txt", "r")
+        lineas=file.readlines()
+        x=0
+        while x <= len(lineas):
+            fila=lineas[x:x+4]
+            self.matriz.append(fila)
+            x=x+4 
 
-    def consultar(self):
+    def consultarLibro(self):
+        self.cargarMatriz()
+        titulo=str(input("Buscar título: "))       
+        for i in range(len(self.matriz)):
+            for j in range(len(self.matriz[i])):
+                if self.matriz[i][0].find(titulo)>=0:
+                    print(self.matriz[i][j], end="")    
+
+    def consultarLista(self):
         if os.path.isfile('C:/Users/formacion/Documents/GitHub/sprint6/lista_libros.txt'):
             f = open("lista_libros.txt", "r")  # lee el archivo si existe
-            for x in f:
-                print(x)
+            print(f.read())
         else:
             print('El no archivo existe.')
+    
+    def eliminarLibro(self):
+        self.cargarMatriz()
+        titulo = str(input("Buscar título: "))
+        for i in range(len(self.matriz)):
+            for j in range(len(self.matriz[i])):
+                if self.matriz[i][0].find(titulo) >= 0:
+                    del self.matriz[i][j]
+                    """lista = self.matriz
+                    f = open("lista_libros.txt", "a+")
+                    for i in lista:
+                        f.write(i)
+                        f.close()"""
+                    print(self.matriz)
+            
+    
+#libro1=Libro()
+#libro1.consultarLibro()
+#libro1.eliminarLibro()
