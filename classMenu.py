5deeb91c599969ef1dcd7f6ae542c4be2fdf1f8e
laliiosorio <57403782+laliiosorio@users.noreply.github.com>
2020-02-06 10:10:49 +0100

    Update classMenu.py

--- classMenu.py
+++ classMenu.py
@@ -5,34 +5,41 @@ class MenuLibros:
     
     def __init__(self):
         self.libro1 = Libro()
-        
+    
+    def validarNumero(self):
+        correcto = False
+        opcion = 0
+        try:
+            opcion = int(input(" Seleccione una opción [1-3:] "))
+            corecto=True
+        except ValueError:
+            print("Oops!  valor no válido. Inténte nuevamente...")
+        return opcion       
         
     def mostrarMenu(self):  
-
-        print(30 * "-", "MENU", 30 * "-")
-        print("1.Consultar Lista de Libros")
-        print("2.Consultar por título")
-        print("3.Agregar nuevo libro a la lista ")
-        print("4. Exit / Salir")
-        print(67 * "-")
     
-        menu=True
-        while menu:
-            #self.mostrarMenu()
-            opcion = int(input(" Seleccione una opción [1-3:] "))
-            print("\n")
+        salir=False
+        opcion=0
+        while not salir: 
+            print(30 * "-", "MENU", 30 * "-")
+            print("1.Consultar Lista de Libros")
+            print("2.Consultar por título")
+            print("3.Agregar nuevo libro a la lista ")
+            print("4. Exit / Salir")
+            print(67 * "-")
+            
+            opcion = self.validarNumero()
         
             if opcion == 1:
+                print("\n")
                 print(20 * "-", "LISTA LIBROS",20 * "-")
                 self.libro1.consultarLista()
-                print("\n")
-                print(20 * "-")
             elif opcion == 2:
                 self.libro1.consultarLibro()
             elif opcion == 3:
                 self.libro1.crearGuardar()
             elif opcion == 4:
-                menu = False               
+                salir = True               
             else:
                 input("Wrong option selection. Enter any key to try again..")
 
