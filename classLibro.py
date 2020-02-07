05dfe335009568b3e7e889f1cae14317e1b150ea
laliiosorio <57403782+laliiosorio@users.noreply.github.com>
2020-02-07 12:59:21 +0100

    Update classLibro.py

--- classLibro.py
+++ classLibro.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 
 class Libro:
     
@@ -21,18 +22,18 @@ class Libro:
             f.write("Título: "+self.titulo + '\n')
             f.write("Autor: " + self.autor + '\n')
             f.write("Fecha edición: "+self.fechaEd + '\n')
-            f.write('\n')
         else:
             print("Libro Existe")
             
     def cargarMatriz(self):
+
         file = open("lista_libros.txt", "r")
         lineas=file.readlines()
         x=0
         while x <= len(lineas):
-            fila=lineas[x:x+4]
+            fila=lineas[x:x+3]
             self.matriz.append(fila)
-            x=x+4
+            x=x+3
 
     def consultarLibro(self):
         self.cargarMatriz()
@@ -45,6 +46,7 @@ class Libro:
                         print(self.matriz[i][j], end="")
         else:
             print('El no archivo existe.')
+        self.matriz = []
 
     def consultarLista(self):
         if os.path.isfile('C:/Users/formacion/Documents/GitHub/sprint6/lista_libros.txt'):
@@ -56,17 +58,22 @@ class Libro:
     def eliminarLibro(self):
         self.cargarMatriz()
         titulo = str(input("Buscar título: "))
+        mat = np.array(self.matriz)
+        print(mat[0][0])
         for i in range(len(self.matriz)):
             for j in range(len(self.matriz[i])):
                 if self.matriz[i][0].find(titulo) >= 0:
-                    del self.matriz[i][j]
-                    """lista = self.matriz
-                    f = open("lista_libros.txt", "a+")
-                    for i in lista:
-                        f.write(i)
-                        f.close()"""
-        print(self.matriz)
-            
+                    mat= np.delete(mat, i, axis=0)
+                    break
+        print(mat)
+        f = open("lista_libros.txt", "w")
+        for i in range(len(mat)):
+        	for j in range(len(mat[i])):
+        		f.write(mat[i][j])
+        f.close()
+        
+        self.matriz = []
+        
     
 #libro1=Libro()
 #libro1.cargarMatriz()
